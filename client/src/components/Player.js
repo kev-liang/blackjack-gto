33e6332d90265dbb2a1bca58f12dd487156edad6
Micheal Yao <kevinman003@gmail.com>
2022-11-18 22:41:33 -0800

    Center cards

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,19 +1,29 @@
 import PlayerCards from "./PlayerCards";
 import "../styles/Player.scss";
 import { connect } from "react-redux";
+import ConstantsFE from "../utils/ConstantsFE";
 
 function Dealer(props) {
   const { cardTotal, players } = props;
 
+  const getContainerWidth = (i) => {
+    if (!players) return;
+    const { shownCards } = players[i];
+    let width = ConstantsFE.CARD_WIDTH * (1 + 0.5 * (shownCards.length - 1));
+    return { width: `${width}px` };
+  };
+
   return (
-    <div className="player-container">
-      {players?.map((player) => (
-        <PlayerCards
-          cards={player.shownCards}
-          key={`table-player-cards-${player.id}`}
-        ></PlayerCards>
+    <div>
+      {players?.map((player, i) => (
+        <div className="player-container" style={getContainerWidth(i)}>
+          <PlayerCards
+            cards={player.shownCards}
+            key={`table-player-cards-${player.id}`}
+          ></PlayerCards>
+          <p className="player-value">{cardTotal}</p>
+        </div>
       ))}
-      <p className="player-value">{cardTotal}</p>
     </div>
   );
 }
