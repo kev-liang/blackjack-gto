9666fea3d95dcfc263fab9a21b3aa26ced3dd6db
Micheal Yao <kevinman003@gmail.com>
2022-11-19 18:59:36 -0800

    Add files

---
+++ client/src/utils/DecisionConstantsFE.js
@@ -0,0 +1,23 @@
+const HIT = "H";
+const STAND = "S";
+const SPLIT = "SP";
+const SPLIT_HIT = "SP_H";
+const DOUBLE = "D";
+const DOUBLE_STAND = "DS";
+const SURRENDER = "SR";
+const STRING_MAP = {
+  HIT: "Hit",
+  STAND: "Stand",
+  SPLIT: "Split",
+  DOUBLE: "Double"
+};
+module.exports = {
+  HIT,
+  STAND,
+  SPLIT,
+  SPLIT_HIT,
+  DOUBLE,
+  DOUBLE_STAND,
+  SURRENDER,
+  STRING_MAP
+};
---
+++ utils/DecisionConstants.js
@@ -0,0 +1,21 @@
+const HIT = "H";
+const STAND = "S";
+const SPLIT = "SP";
+const SPLIT_HIT = "SP_H";
+const DOUBLE = "D";
+const DOUBLE_STAND = "DS";
+const SURRENDER = "SR";
+const SURRENDER_HIT = "SR_H";
+const SURRENDER_STAND = "SR_S";
+
+module.exports = {
+  HIT,
+  STAND,
+  SPLIT,
+  SPLIT_HIT,
+  DOUBLE,
+  DOUBLE_STAND,
+  SURRENDER,
+  SURRENDER_HIT,
+  SURRENDER_STAND
+};
