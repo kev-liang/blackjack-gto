b95e415da3c54480bd698cc981e979412180925c
Micheal Yao <kevinman003@gmail.com>
2022-11-25 12:13:29 -0800

    Add counting checker and update styles

--- client/src/components/CorrectDecision.js
+++ client/src/components/CorrectDecision.js
@@ -2,49 +2,52 @@ import { connect } from "react-redux";
 import ConstantsFE from "../utils/ConstantsFE";
 import DecisionConstantsFE from "../utils/DecisionConstantsFE";
 import "../styles/CorrectDecision.scss";
-
-const getCorrectDecisionText = (playerDecision, correctDecision) => {};
-
-const getDecisionText = (table) => {
-  if (!table || !table.lastDecision) return;
-  let { lastDecision } = table;
-  let { correctDecision } = lastDecision;
-  let playerDecision = lastDecision.playerValues.decision;
-  let correctDecisionString = DecisionConstantsFE.STRING_MAP[correctDecision];
-  let playerDecisionString = DecisionConstantsFE.STRING_MAP[playerDecision];
-
-  let playerAndDealerValues = ` with a ${
-    lastDecision.isSoft ? "soft" : "hard"
-  } ${lastDecision.playerValues.playerValue} against the dealer's ${
-    lastDecision.dealerValue
-  }.`;
-
-  let result = "";
-  if (playerDecision === correctDecision) {
-    return (
-      <p>
-        {"Correct "}
-        <span className="correct-decision">{correctDecisionString}</span>
-        {playerAndDealerValues}
-      </p>
-    );
-  } else {
-    return (
-      <p>
-        {"You chose "}
-        <span className="correct-decision">{playerDecisionString}</span>
-        {" instead of "}
-        <span className="incorrect-decision">{correctDecisionString}</span>
-        {playerAndDealerValues}
-      </p>
-    );
-  }
-};
+import React from "react";
 
 function CorrectDecision(props) {
   const { table } = props;
+  const [decisionText, setDecisionText] = React.useState("");
 
-  return <div className="decision-container">{getDecisionText(table)}</div>;
+  React.useEffect(() => {
+    if (!table || !table.lastDecision) return;
+    setDecisionText(getDecisionText(table));
+  }, [table]);
+
+  const getDecisionText = (table) => {
+    let { lastDecision } = table;
+    let { correctDecision } = lastDecision;
+    let playerDecision = lastDecision.playerValues.decision;
+    let correctDecisionString = DecisionConstantsFE.STRING_MAP[correctDecision];
+    let playerDecisionString = DecisionConstantsFE.STRING_MAP[playerDecision];
+
+    let playerAndDealerValues = ` with a ${
+      lastDecision.isSoft ? "soft" : "hard"
+    } ${lastDecision.playerValues.playerValue} against the dealer's ${
+      lastDecision.dealerValue
+    }.`;
+
+    if (playerDecision === correctDecision) {
+      return (
+        <p>
+          {"Correct "}
+          <span className="correct-decision">{correctDecisionString}</span>
+          {playerAndDealerValues}
+        </p>
+      );
+    } else {
+      return (
+        <p>
+          {"You chose "}
+          <span className="correct-decision">{playerDecisionString}</span>
+          {" instead of "}
+          <span className="incorrect-decision">{correctDecisionString}</span>
+          {playerAndDealerValues}
+        </p>
+      );
+    }
+  };
+
+  return <div className="decision-container">{decisionText}</div>;
 }
 
 const mapStateToProps = (state) => {
