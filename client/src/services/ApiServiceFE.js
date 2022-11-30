a3c3074fc9955b7da26108011b6c2d2de730cbf8
Micheal Yao <kevinman003@gmail.com>
2022-11-29 17:03:22 -0800

    Add files

---
+++ client/src/services/ApiServiceFE.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import Url from "../utils/BackendUrlUtil";
+import TableServiceFE from "./TableServiceFE";
+
+import { updateTableAction } from "../actions/tableActions";
+import { store } from "../store";
+
+class ApiServiceFE {
+  constructor() {
+    this.url = Url;
+  }
+
+  getAndUpdateTable(endpoint) {
+    axios
+      .get(endpoint)
+      .then((res) => {
+        let table = res.data;
+        store.dispatch(updateTableAction(table));
+        TableServiceFE.determineNextAction(table);
+      })
+      .catch((e) => {
+        // handle error
+        console.error("Error getting initial table", e);
+      });
+  }
+
+  patchAndUpdateTable(endpoint) {
+    axios
+      .patch(endpoint)
+      .then((res) => {
+        let table = res.data;
+        store.dispatch(updateTableAction(table));
+      })
+      .catch((e) => {
+        // handle error
+        console.error("Error getting initial table", e);
+      });
+  }
+}
+
+export default new ApiServiceFE();
