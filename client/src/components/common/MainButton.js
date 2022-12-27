8de06eb4b1d4abad1bc0664f456a712bfd869d42
Micheal Yao <kevinman003@gmail.com>
2022-12-26 19:09:27 -0800

    Add files

---
+++ client/src/components/common/MainButton.js
@@ -0,0 +1,38 @@
+import Button from "@mui/material/Button";
+import { useState, useEffect } from "react";
+
+const MainButton = (props) => {
+  const { label, handleClick, disabled, height = 50, width = 110, sx } = props;
+  const [buttonStyles, setButtonStyles] = useState();
+
+  useEffect(() => {
+    const defaultStyles = {
+      height,
+      width
+    };
+    if (!disabled) {
+      setButtonStyles(defaultStyles);
+    } else {
+      setButtonStyles({
+        ...defaultStyles,
+        backgroundColor: "#aaa",
+        pointerEvents: "none"
+      });
+    }
+  }, [disabled]);
+
+  return (
+    <Button
+      onClick={() => handleClick(label)}
+      variant="contained"
+      className={`card-action-button ${
+        disabled ? "card-action-button-disabled" : ""
+      }`}
+      sx={{ ...buttonStyles, ...sx }}
+    >
+      {label}
+    </Button>
+  );
+};
+
+export default MainButton;
